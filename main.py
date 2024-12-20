533539e7ea693b3756b63a128ef8336b0326c67b
yahyathegreat <yahyamardan1234@gmail.com>
2024-12-20 18:27:36 +0500

    h,mmmm

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ set2 = {3, 4, 5, 6}
 result = set1.symmetric_difference(set2)
 
 print(result)
+test_dict = {'a': 1, 'b': 2, 'c': 1, 'd': 3, 'e': 1}
+
+frequency_dict = {}
+
+for value in test_dict.values():
+    if value in frequency_dict:
+        frequency_dict[value] += 1
+    else:
+        frequency_dict[value] = 1
+
+print(frequency_dict)
